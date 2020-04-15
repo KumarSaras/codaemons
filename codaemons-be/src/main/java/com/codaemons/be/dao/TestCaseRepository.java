fe34a4abe8794f438bcd38d97b8eef40f2e1c445
Kumar Saras <saraskiit21@gmail.com>
2020-04-15 19:53:01 +0530

    Fixing typo.

--- codaemons-be/src/main/java/com/codaemons/be/dao/TestCaseRepository.java
+++ codaemons-be/src/main/java/com/codaemons/be/dao/TestCaseRepository.java
@@ -14,6 +14,6 @@ public interface TestCaseRepository extends CrudRepository<TestCase, Integer> {
 	@Query("select tc from TestCase tc where tc.testCaseDefaultFlag=true")
 	public TestCase getDefaultTestCase();
 	
-	@Query("selection tc from TestCase tc where tc.questionID=?1")
+	@Query("select tc from TestCase tc where tc.questionID=?1")
 	public List<TestCase> getTestCasesByQuestionID(int questionID);
 }
